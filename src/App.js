51911ac4967ad7249c618853705b7ff728a368f3
Aba221 <thiawa27@live.fr>
2023-09-12 10:18:27 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'Abdou Dieng',
+        bio: 'Abdou est un expert reconnu dans le domaine de la technologie, il inspire les autres par son dévouement à la science.',
+        imgSrc: 'https://img.freepik.com/photos-premium/jeune-homme-affaires-afro-americain-intelligent-visage-souriant-debout-dans-arriere-plan-flou-bureau-occupe-generative-ai-aig20_31965-117833.jpg?w=740',
+        profession: 'Développeur'
+      },
+      show: false,
+      timeElapsed: 0
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState(prevState => ({
+        timeElapsed: prevState.timeElapsed + 1
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  toggleProfile = () => {
+    this.setState(prevState => ({
+      show: !prevState.show
+    }));
+  };
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { show, elapsedTime } = this.state;
+  
+    return (
+      <div className="App">
+        <h1>Visionneuse de profil</h1>
+        <button className="toggle-button" onClick={this.toggleProfile}>
+          {show ? 'Hide Profile' : 'Show Profile'}
+        </button>
+  
+        {show && (
+          <div className="profile">
+            <h2>{fullName}</h2>
+            <p>{bio}</p>
+            <img src={imgSrc} alt={fullName} />
+            <p>Profession: {profession}</p>
+          </div>
+        )}
+  
+        <p className="elapsed-time">
+          Temps écoulé (seconds): {elapsedTime}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
